eab2c5d80dbe09926b35ed18c5f2937fc355a288
Ivan Diaz <A01700486@itesm.mx>
2020-10-27 15:39:52 -0600

    feature: added the login interface

--- AMMKmobil/lib/main.dart
+++ AMMKmobil/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:best_flutter_ui_templates/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'navigation_home_screen.dart';
+import 'package:best_flutter_ui_templates/cuentas/login.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -29,7 +30,7 @@ class MyApp extends StatelessWidget {
         textTheme: AppTheme.textTheme,
         platform: TargetPlatform.iOS,
       ),
-      home: NavigationHomeScreen(),
+      home: Login(),
     );
   }
 }
