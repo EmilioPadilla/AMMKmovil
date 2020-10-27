478daed0cbe388fbb6755db3a6f9dcc2a09f5722
EmilioPadilla <padillam_@hotmail.com>
2020-10-27 14:06:46 -0600

    feat: set template for AMMK

--- AMMKmobil/lib/model/homelist.dart
+++ AMMKmobil/lib/model/homelist.dart
@@ -13,14 +13,14 @@ class HomeList {
   String imagePath;
 
   static List<HomeList> homeList = [
-    HomeList(
-      imagePath: 'assets/hotel/hotel_booking.png',
-      navigateScreen: HotelHomeScreen(),
-    ),
-    HomeList(
-      imagePath: 'assets/fitness_app/fitness_app.png',
-      navigateScreen: FitnessAppHomeScreen(),
-    ),
+    // HomeList(
+    //   imagePath: 'assets/hotel/hotel_booking.png',
+    //   navigateScreen: HotelHomeScreen(),
+    // ),
+    // HomeList(
+    //   imagePath: 'assets/fitness_app/fitness_app.png',
+    //   navigateScreen: FitnessAppHomeScreen(),
+    // ),
     HomeList(
       imagePath: 'assets/design_course/design_course.png',
       navigateScreen: DesignCourseHomeScreen(),
